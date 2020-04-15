5d7e2a8614704f2f22a77ea89c1b3de0f66f95fe
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-04-14 22:56:05 -0700

    [refactor] Remove docker files and related scripts

--- root/create_validator.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 -u /root/create_validator.py "$(cat /.bls_keys)" "$(cat /.bls_passphrase)" "$(cat /.beacon_endpoint)"
--- root/docker-setup.sh
+++
@@ -1,3 +0,0 @@
-pip3 install .
-chmod +x ./run.sh ./activate.sh ./attach.sh ./balances.sh ./cleanse-bls.py ./create_validator.sh
-chmod +x ./deactivate.sh ./export.sh ./header.sh ./headers.sh ./info.sh ./version.sh
