03275b7d396abddab923af179dc7040781952b52
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-04-16 00:52:25 -0700

    [AutoNode-pypi] Fix versioning

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 import subprocess
 from setuptools import setup
 
-version = subprocess.check_output('git rev-list --count HEAD', shell=True).decode()
-commit = subprocess.check_output('git describe --always --long --dirty', shell=True).decode()
 
 setup(
     name='AutoNode',
-    version=f"v{version}.{commit}",
+    version=f"v{subprocess.check_output('git rev-list --count HEAD', shell=True).decode()}",
     description="AutoNode Python Library (this library is only used inside the sentry docker image)",
     author='Daniel Van Der Maden',
     author_email='daniel@harmony.one',
