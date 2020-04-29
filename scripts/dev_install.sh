3769e688185bb0cbdd41d5aec26cfd41685eac58
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-04-29 07:21:00 -0700

    [dev] Make dev install verbose

--- scripts/dev_install.sh
+++ scripts/dev_install.sh
@@ -8,7 +8,7 @@ source "$DIR/install.sh" source
 check_min_dependencies
 check_and_install_dependencies
 python3 -m pip install "$DIR/../" --user
-install > /dev/null || echo "could not do regular install, could be due to changes, continuing..."
+install || echo "could not do regular install, could be due to changes, continuing..."
 python3 -c "from AutoNode import common; common.save_validator_config()" > /dev/null
 harmony_dir=$(python3 -c "from AutoNode import common; print(common.harmony_dir)")
 cp "$DIR"/../scripts/init.py "$harmony_dir"
