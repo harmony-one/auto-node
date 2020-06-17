7352655f8fdde6561603c5e98c359fea0c01c0d4
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-06-17 02:31:04 -0700

    [exception] Add help message at the end of InvalidWalletPassphrase

--- AutoNode/exceptions.py
+++ AutoNode/exceptions.py
@@ -5,6 +5,8 @@ from .common import (
     save_node_config
 )
 
+from pyhmy import Typgpy
+
 
 class ResetNode(Exception):
     """
@@ -21,3 +23,7 @@ class InvalidWalletPassphrase(InvalidToken):
     """
     Exception raised if passphrase is invalid
     """
+
+    def __init__(self):
+        super(InvalidToken, self).__init__(f"Re-authenticate wallet passphrase with "
+                                           f"'{Typgpy.OKGREEN}auto-node auth-wallet{Typgpy.ENDC}'")
